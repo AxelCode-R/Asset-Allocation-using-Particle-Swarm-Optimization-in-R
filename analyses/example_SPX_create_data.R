f4f5628028e441d9afbea1adc2d4dc2a383b2058
Axel <axelroth2@web.de>
2022-07-14 15:58:53 +0200

    asd

---
+++ analyses/example_SPX_create_data.R
@@ -0,0 +1,113 @@
+
+options(scipen=999)
+options(stringsAsFactors = FALSE)
+options(max.print=100)
+library(lubridate)
+library(alphavantager)
+library(dplyr)
+library(readr)
+library(tidyverse)
+library(rvest)
+library(xts)
+
+
+min_date <- "2005-01-01"
+
+
+# GET COMPOSITION OF STX
+wikispx <- read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+currentconstituents <- wikispx %>%
+  html_node("#constituents") %>%
+  html_table(header = TRUE)
+
+spxchanges <- wikispx %>%
+  html_node("#changes") %>%
+  html_table(header = FALSE, fill = TRUE) %>%
+  filter(row_number() > 2) %>% # First two rows are headers
+  `colnames<-`(c('Date','AddTicker','AddName','RemovedTicker','RemovedName','Reason')) %>%
+  mutate(Date = as.Date(Date, format = "%B %d, %Y"),
+         year = year(Date),
+         month = month(Date))
+
+
+# Start at the current constituents…
+currentmonth <- as.Date(format(Sys.Date(), '%Y-%m-01'))
+monthseq <- seq.Date(as.Date('1990-01-01'), currentmonth, by = 'month') %>% rev()
+
+spxstocks <- currentconstituents %>% mutate(Date = currentmonth) %>% select(Date, Ticker = Symbol, Name = Security)
+lastrunstocks <- spxstocks
+
+# Iterate through months, working backwards
+for (i in 2:length(monthseq)) {
+  d <- monthseq[i]
+  y <- year(d)
+  m <- month(d)
+  changes <- spxchanges %>%
+    filter(year == year(d), month == month(d))
+
+  # Remove added tickers (we’re working backwards in time, remember)
+  tickerstokeep <- lastrunstocks %>%
+    anti_join(changes, by = c("Ticker" = "AddTicker")) %>%
+    mutate(Date = d)
+
+  # Add back the removed tickers…
+  tickerstoadd <- changes %>%
+    filter(!RemovedTicker == "") %>%
+    transmute(Date = d,
+              Ticker = RemovedTicker,
+              Name = RemovedName)
+
+  thismonth <- tickerstokeep %>% bind_rows(tickerstoadd)
+  spxstocks <- spxstocks %>% bind_rows(thismonth)
+
+  lastrunstocks <- thismonth
+}
+spx_composition <- spxstocks %>% filter(Date >= as.Date(min_date))
+
+save(spx_composition, file="data/spx_composition.rdata")
+
+
+
+# GET DAILY RETURNS AND PRICES FOR ALL SPX TICKERS
+api_key <- Sys.getenv("api_key")
+source("R/alphavantage_wrappers.R")
+spx_daily_adj_data <- get_prices_and_returns_av(
+    choosen_tickers = spx_composition$Ticker %>% unique(),
+    min_date = as.Date(min_date),
+    max_date = Sys.Date(),
+    min_date_history = days(0)
+  )
+
+save(spx_daily_adj_data, file="data/spx_daily_adj_data.rdata")
+
+
+
+# GET REFERENCE INDEX (iShares S&P 500 UCITS Dist ETF)
+spx_index_daily_adj_data <- get_prices_and_returns_av(
+  choosen_tickers = "IUSA.DEX",
+  min_date = as.Date(min_date),
+  max_date = Sys.Date(),
+  min_date_history = days(0)
+)
+
+save(spx_index_daily_adj_data, file="data/spx_index_daily_adj_data.rdata")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
