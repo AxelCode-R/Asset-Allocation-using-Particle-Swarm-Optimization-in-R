aef92d1b1d9e4408570ee8775a4602b944cedf94
Axel <axelroth2@web.de>
2022-09-10 12:48:24 +0200

    asd

--- analyses/SP500_BPSO_QP_und_PSO.R
+++ analyses/SP500_BPSO_QP_und_PSO.R
@@ -50,99 +50,11 @@ mat <- list(
   ),
   meq = 1
 )
-# mat$Dmat_nPD <- as.matrix(nearPD(cov(mat$Dmat))$mat)
-#
-# res_bpso <- bpsoptim(
-#   par = sample(c(rep(1, 30), rep(0, ncol(pool_returns_raw)-30))),
-#   fn = function(x){
-#     x <- as.logical(x)
-#     if(sum(x)>30){
-#       return(10+sum(x)-30)
-#     }
-#     qp <- solve.QP(
-#       Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{mat$Dmat_nPD[x,x]}, dvec = mat$dvec[x],
-#       Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
-#     )
-#     real_fit <- as.numeric(0.5 * t(qp$solution) %*% mat$Dmat[x,x] %*% qp$solution - t(mat$dvec[x]) %*% qp$solution)
-#     return(real_fit)
-#   },
-#   control = list(
-#     trace = T,
-#     maxit = 200
-#   )
-# )
-#
-#
-# fn = function(x){
-#   x <- as.logical(x)
-#   qp <- solve.QP(
-#     Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{mat$Dmat_nPD[x,x]}, dvec = mat$dvec[x],
-#     Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
-#   )
-#   real_fit <- as.numeric(0.5 * t(qp$solution) %*% mat$Dmat[x,x] %*% qp$solution - t(mat$dvec[x]) %*% qp$solution)
-#   return(real_fit)
-# }
-#
-#
-# X <- apply(mrunif(nr = ncol(pool_returns_raw), nc = 100, lower = 0, upper = 1), 2, discretize_to)
-# X_fit <- apply(X, 2, fn)
-#
-# P <- X
-# P_fit <- X_fit
-# p_g <- P[, which.min(P_fit)]
-# p_g_fit <- which.min(P_fit)
-#
-#
-#
-#
-#
-# mat <- list(
-#   Dmat = cov(pool_returns_raw),
-#   dvec = cov(pool_returns_raw, bm_returns),
-#   Amat = t(rbind(
-#     rep(1, ncol(pool_returns_raw)), # sum up to 1
-#     diag(1,
-#          nrow=ncol(pool_returns_raw),
-#          ncol=ncol(pool_returns_raw)) # long only
-#   )),
-#   bvec = c(
-#     1, # sum up to 1
-#     rep(0, ncol(pool_returns_raw)) # long only
-#   ),
-#   meq = 1
-# )
-#
-# discretize_to <- function(x, n=30){
-#   res <- rep(0, length(x))
-#   res[order(x, decreasing = T)[1:n]] <- 1
-#   return(res)
-# }
-#
-# res_pso <- psoptim(
-#   par = rep(NA, ncol(pool_returns_raw)),
-#   fn = function(x){
-#     x <- discretize_to(x, n=30)
-#     qp <- solve.QP(
-#       Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{as.matrix(nearPD(mat$Dmat[x,x])$mat)}, dvec = mat$dvec[x],
-#       Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
-#     )
-#     real_fit <- as.numeric(0.5 * t(qp$solution) %*% mat$Dmat[x,x] %*% qp$solution - t(mat$dvec[x]) %*% qp$solution)
-#     return(real_fit)
-#   },
-#   upper=1,
-#   lower=0,
-#   control = list(
-#     trace = T,
-#     maxit = 200
-#   )
-# )
-#
 
 
 
-#install.packages("GA")
 
-N <- 5
+
 
 discretize_to <- function(x, n){
   res <- rep(0, length(x))
@@ -151,135 +63,152 @@ discretize_to <- function(x, n){
 }
 
 
-# VS naive
-time_naive <- system.time({
-  itp <- function(pool_returns, bm_returns){
-    mat <- list(
-      Dmat = cov(pool_returns),
-      dvec = cov(pool_returns, bm_returns),
-      Amat = t(rbind(
-        rep(1, ncol(pool_returns)), # sum up to 1
-        diag(1,
-             nrow=ncol(pool_returns),
-             ncol=ncol(pool_returns)) # long only
-      )),
-      bvec = c(
-        1, # sum up to 1
-        rep(0, ncol(pool_returns)) # long only
-      ),
-      meq = 1
-    )
 
-    qp <- solve.QP(
-      Dmat = mat$Dmat, dvec = mat$dvec,
-      Amat = mat$Amat, bvec = mat$bvec, meq = mat$meq
-    )
+info <- NULL
 
-    res <- list(
-      "fit" = qp$value,
-      "var" = as.numeric(
-        var(pool_returns %*% qp$solution - bm_returns)),
-      "solution" = setNames(qp$solution, colnames(pool_returns))
-    )
-  }
+for(k in seq(10, 100, 10)){
+  N <- k
+  # VS naive
+  time_naive <- system.time({
+    itp <- function(pool_returns, bm_returns){
+      mat <- list(
+        Dmat = cov(pool_returns),
+        dvec = cov(pool_returns, bm_returns),
+        Amat = t(rbind(
+          rep(1, ncol(pool_returns)), # sum up to 1
+          diag(1,
+               nrow=ncol(pool_returns),
+               ncol=ncol(pool_returns)) # long only
+        )),
+        bvec = c(
+          1, # sum up to 1
+          rep(0, ncol(pool_returns)) # long only
+        ),
+        meq = 1
+      )
 
-  res <- NULL
-  step_size <- 1
-  n_assets <- unique(c(seq(ncol(pool_returns_raw), N, -step_size), N))
-  pool_returns_naive <- pool_returns_raw
-  for(i in n_assets){
+      qp <- solve.QP(
+        Dmat = mat$Dmat, dvec = mat$dvec,
+        Amat = mat$Amat, bvec = mat$bvec, meq = mat$meq
+      )
 
-    temp <- if(i==max(n_assets)){
-      itp(pool_returns_naive, bm_returns)
-    }else{
-      itp(
-        pool_returns_naive[, names(sort(temp$solution, decreasing = T)[1:i])],
-        bm_returns
+      res <- list(
+        "fit" = qp$value,
+        "var" = as.numeric(
+          var(pool_returns %*% qp$solution - bm_returns)),
+        "solution" = setNames(qp$solution, colnames(pool_returns))
       )
     }
-    res <- rbind(
-      res,
-      data.frame("N"=i, "fit"=temp$fit, row.names = NULL)
+
+    res <- NULL
+    step_size <- 10
+    n_assets <- unique(c(seq(ncol(pool_returns_raw), N, -step_size), N))
+    pool_returns_naive <- pool_returns_raw
+    for(i in n_assets){
+
+      temp <- if(i==max(n_assets)){
+        itp(pool_returns_naive, bm_returns)
+      }else{
+        itp(
+          pool_returns_naive[, names(sort(temp$solution, decreasing = T)[1:i])],
+          bm_returns
+        )
+      }
+      res <- rbind(
+        res,
+        data.frame("N"=i, "fit"=temp$fit, row.names = NULL)
+      )
+    }
+
+    res_naive <- last(res)$fit
+  })
+
+
+
+  # GA approach
+  time_ga_fixed <- system.time({
+    fn <- function(x){
+      if(sum(x) > N || sum(x) <= 2){return(0)}
+      x <- as.logical(x)
+      qp <- solve.QP(
+        Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{as.matrix(nearPD(mat$Dmat[x,x])$mat)}, dvec = mat$dvec[x],
+        Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
+      )
+      real_fit <- as.numeric(0.5 * t(qp$solution) %*% mat$Dmat[x,x] %*% qp$solution - t(mat$dvec[x]) %*% qp$solution)
+      return(-real_fit)
+    }
+    res_ga <- ga(
+      type = "binary",
+      fitness = fn,
+      nBits = ncol(pool_returns_raw),
+      crossover = gabin_uCrossover,
+      popSize = 300,
+      run=100,
+      maxiter = 500,
+      maxFitness = -last(res)$fit,
+      suggestions=t(as.matrix(apply(mrunif(nr = ncol(pool_returns_raw), nc = 100, lower = 0, upper = 1), 2, discretize_to, n=N))),
+      monitor = F
     )
-  }
-
-  res_naive <- last(res)$fit
-})
 
 
-
-# GA approach
-time_ga_fixed <- system.time({
-  fn <- function(x){
-    if(sum(x) > N || sum(x) <= 2){return(0)}
-    x <- as.logical(x)
+    x <- as.logical(res_ga@solution)
     qp <- solve.QP(
       Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{as.matrix(nearPD(mat$Dmat[x,x])$mat)}, dvec = mat$dvec[x],
       Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
     )
-    real_fit <- as.numeric(0.5 * t(qp$solution) %*% mat$Dmat[x,x] %*% qp$solution - t(mat$dvec[x]) %*% qp$solution)
-    return(-real_fit)
-  }
-  res_ga <- ga(
-    type = "binary",
-    fitness = fn,
-    nBits = ncol(pool_returns_raw),
-    crossover = gabin_uCrossover,
-    popSize = 300,
-    run=100,
-    maxiter = 500,
-    maxFitness = -last(res)$fit,
-    suggestions=t(as.matrix(apply(mrunif(nr = ncol(pool_returns_raw), nc = 100, lower = 0, upper = 1), 2, discretize_to, n=N)))
-  )
+    res_ga_fixed <- qp$value
 
-
-  x <- as.logical(res_ga@solution)
-  qp <- solve.QP(
-    Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{as.matrix(nearPD(mat$Dmat[x,x])$mat)}, dvec = mat$dvec[x],
-    Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
-  )
-  res_ga_fixed <- qp$value
-
-})
+  })
 
 
 
 
-time_ga_long <- system.time({
-  fn <- function(x){
-    if(sum(x) > N || sum(x) <= 2){return(0)}
-    x <- as.logical(x)
-    qp <- solve.QP(
-      Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{as.matrix(nearPD(mat$Dmat[x,x])$mat)}, dvec = mat$dvec[x],
-      Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
+  # time_ga_long <- system.time({
+  #   fn <- function(x){
+  #     if(sum(x) > N || sum(x) <= 2){return(0)}
+  #     x <- as.logical(x)
+  #     qp <- solve.QP(
+  #       Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{as.matrix(nearPD(mat$Dmat[x,x])$mat)}, dvec = mat$dvec[x],
+  #       Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
+  #     )
+  #     real_fit <- as.numeric(0.5 * t(qp$solution) %*% mat$Dmat[x,x] %*% qp$solution - t(mat$dvec[x]) %*% qp$solution)
+  #     return(-real_fit)
+  #   }
+  #   res_ga <- ga(
+  #     type = "binary",
+  #     fitness = fn,
+  #     nBits = ncol(pool_returns_raw),
+  #     crossover = gabin_uCrossover,
+  #     popSize = 500,
+  #     run=100,
+  #     maxiter = 500,
+  #     suggestions=t(as.matrix(apply(mrunif(nr = ncol(pool_returns_raw), nc = 100, lower = 0, upper = 1), 2, discretize_to, n=N)))
+  #   )
+  #
+  #
+  #   x <- as.logical(res_ga@solution)
+  #   qp <- solve.QP(
+  #     Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{as.matrix(nearPD(mat$Dmat[x,x])$mat)}, dvec = mat$dvec[x],
+  #     Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
+  #   )
+  #   res_ga_long <- qp$value
+  #
+  # })
+  #
+  #
+  # data.frame(
+  #   "type" = c("naive", "GA_fixed", "GA_long"),
+  #   "fit" = c(res_naive, res_ga_fixed, res_ga_long),
+  #   "time" = c(time_naive[3], time_ga_fixed[3], time_ga_long[3])
+  # )
+
+
+  info <- rbind(
+    info,
+    data.frame(
+      "type" = c("naive", "GA_fixed"),
+      "fit" = c(res_naive, res_ga_fixed),
+      "time" = c(time_naive[3], time_ga_fixed[3])
     )
-    real_fit <- as.numeric(0.5 * t(qp$solution) %*% mat$Dmat[x,x] %*% qp$solution - t(mat$dvec[x]) %*% qp$solution)
-    return(-real_fit)
-  }
-  res_ga <- ga(
-    type = "binary",
-    fitness = fn,
-    nBits = ncol(pool_returns_raw),
-    crossover = gabin_uCrossover,
-    popSize = 500,
-    run=100,
-    maxiter = 300,
-    suggestions=t(as.matrix(apply(mrunif(nr = ncol(pool_returns_raw), nc = 100, lower = 0, upper = 1), 2, discretize_to, n=N)))
   )
-
-
-  x <- as.logical(res_ga@solution)
-  qp <- solve.QP(
-    Dmat = if(is.positive.definite(mat$Dmat[x,x])){mat$Dmat[x,x]}else{as.matrix(nearPD(mat$Dmat[x,x])$mat)}, dvec = mat$dvec[x],
-    Amat = mat$Amat[x, ], bvec = mat$bvec, meq = mat$meq
-  )
-  res_ga_long <- qp$value
-
-})
-
-
-data.frame(
-  "type" = c("naive", "GA_fixed", "GA_long"),
-  "fit" = c(res_naive, res_ga_fixed, res_ga_long),
-  "time" = c(time_naive[3], time_ga_fixed[3], time_ga_long[3])
-)
+}
