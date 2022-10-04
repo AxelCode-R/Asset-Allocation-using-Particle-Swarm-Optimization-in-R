10130a051d205f6c39d1ac65362d933697de25c3
Axel <axelroth2@web.de>
2022-10-04 11:41:53 +0200

    sss

--- R/PSO_functions.R
+++ R/PSO_functions.R
@@ -471,6 +471,151 @@ pso_local <- function(
 
 
 
+pso_self_adaptive_velocity <- function(
+    par,
+    fn,
+    lower,
+    upper,
+    control = list()
+){
+
+  # use default control values if not set
+  control_ = list(
+    s = 10, # swarm size
+    c.p = 0.5, # inherit best
+    c.g = 0.5, # global best
+    maxiter = 200, # iterations
+    w0 = 1.2, # starting inertia weight
+    wN = 0, # ending inertia weight
+    save_traces = F, # save more information
+    save_fit = F,
+    Sp = 0.8, # selection probability of velocity strategy
+    Cp = 0.7 # selection probability of boundary operations
+  )
+  control <- c(control, control_[!names(control_) %in% names(control)])
+
+
+
+
+  # init data-structure
+  X <- mrunif(
+    nr = length(par), nc=control$s, lower=lower, upper=upper
+  )
+  if(all(!is.na(par))){
+    X[, 1] <- par
+  }
+  X_fit <- apply(X, 2, fn)
+  V <- mrunif(
+    nr = length(par), nc=control$s,
+    lower=-(upper-lower), upper=(upper-lower)
+  )/10
+  P <- X
+  P_fit <- X_fit
+  p_g <- P[, which.min(P_fit)]
+  p_g_fit <- min(P_fit)
+
+  ac_params <- data.frame("w"=rep(0.5, length(par)), "c.p"=rep(2, length(par)), "c.g"=rep(2, length(par)))
+
+  trace_data <- NULL
+  fit_data <- NULL
+  for(i in 1:control$maxiter){
+
+    mu1 <- 0.1*(1-(i/control$maxiter)^2)+0.3
+    sig1 <- 0.1
+    mu2 <- 0.4*(1-(i/control$maxiter)^2)+0.2
+    sig2 <- 0.4
+
+    for(p in 1:control$s){
+      if(runif(1) > control$Sp){
+        V[,p] <- ac_params[p,]$w * V[,p] +
+          ac_params[p,]$c.p * runif(1) * (P[,p]-X[,p]) +
+          ac_params[p,]$c.g * runif(1) * (p_g-X[,p])
+      }else{
+        if(runif(1) < 0.5){
+          V[,p] <- ac_params[p,]$w * V[,p] +
+            ac_params[p,]$c.p * rcauchy(1, mu1, sig1) * (P[,p]-X[,p]) +
+            ac_params[p,]$c.g * rcauchy(1, mu1, sig1) * (p_g-X[,p])
+        }else{
+          V[,p] <- ac_params[p,]$w * V[,p] +
+            ac_params[p,]$c.p * rcauchy(1, mu2, sig2) * (P[,p]-X[,p]) +
+            ac_params[p,]$c.g * rcauchy(1, mu2, sig2) * (p_g-X[,p])
+        }
+      }
+    }
+    X <- X + V
+
+    upper_breaks <- X > upper
+    ub_ind <- which(upper_breaks==T, arr.ind = T)
+    if(nrow(ub_ind)>0){
+      for(k in 1:nrow(ub_ind)){
+        if(runif(1) > control$Cp){
+          X[ub_ind[k,1],ub_ind[k,2]] <- runif(1, lower, upper)
+        }else{
+          X[ub_ind[k,1],ub_ind[k,2]] <- upper
+        }
+      }
+    }
+
+    lower_breaks <- X < lower
+    lb_ind <- which(lower_breaks==T, arr.ind = T)
+    if(nrow(lb_ind)>0){
+      for(k in 1:nrow(lb_ind)){
+        if(runif(1) > control$Cp){
+          X[lb_ind[k,1],lb_ind[k,2]] <- runif(1, lower, upper)
+        }else{
+          X[lb_ind[k,1],lb_ind[k,2]] <- lower
+        }
+      }
+    }
+
+
+    # # set velocity to zeros if not in valid space
+    # V[X > upper] <- 0#-V[X > upper]
+    # V[X < lower] <- 0#-V[X < lower]
+    #
+    # # move into valid space
+    # X[X > upper] <- upper
+    # X[X < lower] <- lower
+
+    # evaluate objective function
+    X_fit <- apply(X, 2, fn)
+
+    # save new previews best
+    P[, P_fit > X_fit] <- X[, P_fit > X_fit]
+    P_fit[P_fit > X_fit] <- X_fit[P_fit > X_fit]
+
+    # save new global best
+    if(any(P_fit < p_g_fit)){
+      p_g <- P[, which.min(P_fit)]
+      p_g_fit <- min(P_fit)
+    }
+
+    if(control$save_traces){
+      trace_data <- rbind(trace_data, data.frame("iter"=i, t(X)))
+    }
+    if(control$save_fit){
+      fit_data <- rbind(fit_data, data.frame("iter"=i, "mean"=mean(P_fit), "best"=p_g_fit))
+    }
+  }
+
+  res <- list(
+    "solution" = p_g,
+    "fitness" = p_g_fit
+  )
+  if(control$save_traces){
+    res$trace_data <- trace_data
+  }
+  if(control$save_fit){
+    res$fit_data <- fit_data
+  }
+  return(res)
+}
+
+
+
+
+
+
 # pso_preserving_feasibility <- function(
 #     par,
 #     fn_fit,
