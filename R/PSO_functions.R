007f22cd32236c5ac408425886f2d7da837328d4
Axel <axelroth2@web.de>
2022-11-10 15:43:01 +0100

    asd

--- R/PSO_functions.R
+++ R/PSO_functions.R
@@ -630,98 +630,103 @@ pso_self_adaptive_velocity <- function(
 
 
 
-# pso_preserving_feasibility <- function(
-#     par,
-#     fn_fit,
-#     fn_const,
-#     lower,
-#     upper,
-#     control = list()
-# ){
-#
-#   # use default control values if not set
-#   control_ = list(
-#     s = 10, # swarm size
-#     c.p = 0.5, # inherit best
-#     c.g = 0.5, # global best
-#     maxiter = 200, # iterations
-#     w0 = 1.2, # starting inertia weight
-#     wN = 0, # ending inertia weight
-#     save_traces = F, # save more information
-#     save_fit = F
-#   )
-#   control <- c(control, control_[!names(control_) %in% names(control)])
-#
-#   # init data-structure
-#   X <- mrunif(
-#     nr = length(par), nc=control$s, lower=lower, upper=upper
-#   )
-#   if(all(!is.na(par))){
-#     X[, 1] <- par
-#   }
-#   X_fit <- apply(X, 2, fn)
-#   V <- mrunif(
-#     nr = length(par), nc=control$s,
-#     lower=-(upper-lower), upper=(upper-lower)
-#   )/10
-#   P <- X
-#   P_fit <- X_fit
-#   p_g <- P[, which.min(P_fit)]
-#   p_g_fit <- min(P_fit)
-#
-#
-#   trace_data <- NULL
-#   fit_data <- NULL
-#   for(i in 1:control$maxiter){
-#
-#     # move particles
-#     V <-
-#       (control$w0-(control$w0-control$wN)*i/control$maxiter) * V +
-#       control$c.p * runif(length(par)) * (P-X) +
-#       control$c.g * runif(length(par)) * (p_g-X)
-#     X <- X + V
-#
-#     # set velocity to zeros if not in valid space
-#     V[X > upper] <- 0#-V[X > upper]
-#     V[X < lower] <- 0#-V[X < lower]
-#
-#     # move into valid space
-#     X[X > upper] <- upper
-#     X[X < lower] <- lower
-#
-#     # evaluate objective function
-#     X_fit <- apply(X, 2, fn)
-#
-#     # save new previews best
-#     P[, P_fit > X_fit] <- X[, P_fit > X_fit]
-#     P_fit[P_fit > X_fit] <- X_fit[P_fit > X_fit]
-#
-#     # save new global best
-#     if(any(P_fit < p_g_fit)){
-#       p_g <- P[, which.min(P_fit)]
-#       p_g_fit <- min(P_fit)
-#     }
-#
-#     if(control$save_traces){
-#       trace_data <- rbind(trace_data, data.frame("iter"=i, t(X)))
-#     }
-#     if(control$save_fit){
-#       fit_data <- rbind(fit_data, data.frame("iter"=i, "mean"=mean(P_fit), "best"=p_g_fit))
-#     }
-#   }
-#
-#   res <- list(
-#     "solution" = p_g,
-#     "fitness" = p_g_fit
-#   )
-#   if(control$save_traces){
-#     res$trace_data <- trace_data
-#   }
-#   if(control$save_fit){
-#     res$fit_data <- fit_data
-#   }
-#   return(res)
-# }
+pso_preserving_feasibility <- function(
+    par,
+    fn_fit,
+    fn_const,
+    lower,
+    upper,
+    control = list()
+){
+
+  # use default control values if not set
+  control_ = list(
+    s = 10, # swarm size
+    c.p = 0.5, # inherit best
+    c.g = 0.5, # global best
+    maxiter = 200, # iterations
+    w0 = 1.2, # starting inertia weight
+    wN = 0, # ending inertia weight
+    save_traces = F, # save more information
+    save_fit = F
+  )
+  control <- c(control, control_[!names(control_) %in% names(control)])
+
+  # init data-structure
+  X <- mrunif(
+    nr = length(par), nc=control$s, lower=lower, upper=upper
+  )
+  if(all(!is.na(par))){
+    X[, 1] <- par
+  }
+  X_fit <- apply(X, 2, fn_fit)
+  X_const <- apply(X, 2, fn_const)
+
+  V <- mrunif(
+    nr = length(par), nc=control$s,
+    lower=-(upper-lower), upper=(upper-lower)
+  )/10
+  P <- X
+  P_fit <- X_fit
+  P_const <- X_const
+  p_g <- P[, which.min(P_fit + 10^6*P_const!=0)]
+  p_g_fit <- P_fit[which.min(P_fit + 10^6*P_const!=0)]
+
+
+  trace_data <- NULL
+  fit_data <- NULL
+  for(i in 1:control$maxiter){
+
+    # move particles
+    V <-
+      (control$w0-(control$w0-control$wN)*i/control$maxiter) * V +
+      control$c.p * runif(length(par)) * (P-X) +
+      control$c.g * runif(length(par)) * (p_g-X)
+    X <- X + V
+
+    # set velocity to zeros if not in valid space
+    V[X > upper] <- 0#-V[X > upper]
+    V[X < lower] <- 0#-V[X < lower]
+
+    # move into valid space
+    X[X > upper] <- upper
+    X[X < lower] <- lower
+
+    # evaluate objective function
+    X_fit <- apply(X, 2, fn_fit)
+    X_const <- apply(X, 2, fn_const)
+
+    # save new previews best
+    P[, P_fit > X_fit & X_const == 0] <- X[, P_fit > X_fit & X_const == 0]
+    P_const[P_fit > X_fit & X_const == 0] <- X_const[P_fit > X_fit & X_const == 0]
+    P_fit[P_fit > X_fit & X_const == 0] <- X_fit[P_fit > X_fit & X_const == 0]
+
+    # save new global best
+    if(any(P_fit < p_g_fit & P_const == 0)){
+      p_g <- P[, which.min(P_fit + 10^6*P_const!=0)]
+      p_g_fit <- P_fit[which.min(P_fit + 10^6*P_const!=0)]
+    }
+
+    if(control$save_traces){
+      trace_data <- rbind(trace_data, data.frame("iter"=i, t(X)))
+    }
+    if(control$save_fit){
+      fit_data <- rbind(fit_data, data.frame("iter"=i, "mean"=mean(P_fit), "best"=p_g_fit))
+    }
+  }
+
+  res <- list(
+    "solution" = p_g,
+    "fitness" = p_g_fit
+  )
+  if(control$save_traces){
+    res$trace_data <- trace_data
+  }
+  if(control$save_fit){
+    res$fit_data <- fit_data
+  }
+  return(res)
+}
 
 
 
